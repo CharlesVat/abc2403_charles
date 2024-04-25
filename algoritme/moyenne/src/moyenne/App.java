96ef2ced118401b8608c3c0f8037d671e27c1ae7
Charles <166002369+CharlesVat@users.noreply.github.com>
2024-04-25 08:37:31 +0200

    exercice 1.4

--- algoritme/moyenne/src/moyenne/App.java
+++ algoritme/moyenne/src/moyenne/App.java
@@ -1,11 +1,25 @@
 package moyenne;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		long rayon;
+		long aire;
+		long volume;
+		Scanner sc;
 		
+		sc = new Scanner(System.in);
 		
+		System.out.println("inserez le rayon :");
+		
+		rayon= sc.nextLong();
+		aire = (long) (4 * 3.14 * (rayon * rayon));
+		volume =(long) (4 * 3.14 * (rayon * rayon * rayon));
+		
+		System.out.println(aire);
+		System.out.println(volume);
 	}
 
 }
