14d520b9d63b6cd257dade4b27ba28e88986d812
Charles <166002369+CharlesVat@users.noreply.github.com>
2024-04-25 09:55:52 +0200

    exo 1.6

---
+++ algoritme/inversion/src/inversion/App.java
@@ -0,0 +1,33 @@
+package inversion;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		long ChiffreA;
+		long ChiffreB;
+		long Melangeur;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+	
+		System.out.println("Saisissez un chiffre A: ");
+		
+		ChiffreA = sc.nextLong();
+		
+		System.out.println("Saisissez un chiffre B");
+
+		ChiffreB = sc.nextLong();
+		
+		System.out.println("vous avez choisi: "+ ChiffreA +" et " + ChiffreB);
+		
+		Melangeur = ChiffreA;
+		ChiffreA= ChiffreB;
+		ChiffreB= Melangeur;
+		
+		System.out.println("une fois inversé ca fait: "+ ChiffreA +" et " + ChiffreB);
+		
+	}
+
+}
